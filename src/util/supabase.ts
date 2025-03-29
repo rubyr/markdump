05c1d7bc9c22b9d07a5483f6b25e1477237a0347
Ruby Rinken <rubyrinken@gmail.com>
2025-03-29 00:44:21 -0400

    perhaps a different approach would be more fruitful

--- src/util/supabase.ts
+++ src/util/supabase.ts
@@ -1,9 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './supabase.types';
 
+const SUPABASE_URL = import.meta.env.SUPABASE_URL || process.env.SUPABASE_URL;
+const SUPABASE_SECRET = import.meta.env.SUPABASE_SECRET || process.env.SUPABASE_SECRET;
+
 const supabase = createClient<Database>(
-    import.meta.env.SUPABASE_URL,
-    import.meta.env.SUPABASE_SECRET
+    SUPABASE_URL,
+    SUPABASE_SECRET
 );
 
 export default supabase;
