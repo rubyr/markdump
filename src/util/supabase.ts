3a3ff98746b75c9f685e359b08f5c3991ef57c21
Ruby Rinken <rubyrinken@gmail.com>
2025-03-29 01:31:25 -0400

    dotenv did not work

--- src/util/supabase.ts
+++ src/util/supabase.ts
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './supabase.types';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const SUPABASE_URL = import.meta.env.SUPABASE_URL || process.env.SUPABASE_URL;
-const SUPABASE_SECRET = import.meta.env.SUPABASE_SECRET || process.env.SUPABASE_SECRET;
+const SUPABASE_URL = process.env.SUPABASE_URL || import.meta?.env?.SUPABASE_URL;
+const SUPABASE_SECRET = process.env.SUPABASE_SECRET || import.meta?.env?.SUPABASE_SECRET;
 
 const supabase = createClient<Database>(
     SUPABASE_URL,
