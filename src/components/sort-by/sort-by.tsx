5403f7c09f8b8ec64c8595996cace0c8310d884a
Ruby Rinken <rubyrinken@gmail.com>
2025-04-02 23:57:27 -0400

    add more specific types + documentation to sort functions

--- src/components/sort-by/sort-by.tsx
+++ src/components/sort-by/sort-by.tsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from "preact/hooks";
-import styles from "./sort-by.module.css";
+import { useEffect, useState } from 'preact/hooks';
+import styles from './sort-by.module.css';
+import type { ChangeEvent } from 'preact/compat';
+import type { PostgrestFilterBuilder } from '@supabase/postgrest-js';
+import type { GenericSchema } from '@supabase/supabase-js/dist/module/lib/types';
 
 export const sortOptions = {
     newest: {
@@ -21,36 +24,57 @@ export const sortOptions = {
         name: 'Title (Z-A)',
         col: 'title',
         ascending: false,
-    }
-}
+    },
+};
 export type SortType = keyof typeof sortOptions;
 
-export const sortPostQuery = (query:any, sortBy: SortType) => {
-    const {col,ascending} = sortOptions[sortBy] ?? sortOptions.newest;
+/**
+ * Calls the appropriate sort method on the Supabase query, based on the sort
+ * provided.
+ * @param query The query to sort. Call this *after* any filters, and *before*
+ * any modifiers.
+ * @param sortBy How to sort the data.
+ * @returns 
+ */
+export const sortPostQuery = <
+    S extends GenericSchema,
+    Rw extends Record<string, unknown>,
+    Rs
+>(
+    query: PostgrestFilterBuilder<S, Rw, Rs>,
+    sortBy: SortType
+) => {
+    const { col, ascending } = sortOptions[sortBy] ?? sortOptions.newest;
     return query.order(col, { ascending });
-}
+};
 
 const SortBy = ({}) => {
     const [currentSortOption, setCurrentSortOption] = useState('');
     useEffect(() => {
-        setCurrentSortOption(new URLSearchParams(window.location.search).get('sort') ?? '');
-    },[]);
-    const selectSortOption = (e:any) => {
+        setCurrentSortOption(
+            new URLSearchParams(window.location.search).get('sort') ?? ''
+        );
+    }, []);
+    const selectSortOption = (e: ChangeEvent) => {
         const searchParams = new URLSearchParams(window.location.search);
-        searchParams.set('sort',(e.target as HTMLSelectElement).value ?? Object.keys(sortOptions)[0]);
+        searchParams.set(
+            'sort',
+            (e.target as HTMLSelectElement).value ?? Object.keys(sortOptions)[0]
+        );
         window.location.search = searchParams.toString();
-    }
+    };
 
-    return (<div class={styles.sortContainer}>
-        <label for="sort">Sort By</label>
-        <select name="sort" onChange={selectSortOption}>
-            {Object.keys(sortOptions).map(key => (
-                <option value={key} selected={key === currentSortOption}>
-                    {sortOptions[key as SortType].name}
-                </option>
-            ))}
-        </select>
-    </div>
+    return (
+        <div class={styles.sortContainer}>
+            <label for="sort">Sort By</label>
+            <select name="sort" onChange={selectSortOption}>
+                {Object.keys(sortOptions).map((key) => (
+                    <option value={key} selected={key === currentSortOption}>
+                        {sortOptions[key as SortType].name}
+                    </option>
+                ))}
+            </select>
+        </div>
     );
 };
 
