d11ae53041079bb85e8689c9ee15760df4f00d3c
Sciman101 <Sciman101@proton.me>
2025-04-13 21:06:51 -0700

    Actually filter the post lists by 'adult'

--- src/components/sort-by/show-adult.tsx
+++ src/components/sort-by/show-adult.tsx
@@ -1,6 +1,31 @@
 import { useEffect, useState } from 'preact/hooks';
 import styles from './sort-by.module.css';
 import type { ChangeEvent } from 'preact/compat';
+import type { PostgrestFilterBuilder } from '@supabase/postgrest-js';
+import type { GenericSchema } from '@supabase/supabase-js/dist/module/lib/types';
+
+/**
+ * Calls the appropriate sort method on the Supabase query, based on the sort
+ * provided.
+ * @param query The query to sort. Call this *after* any filters, and *before*
+ * any modifiers.
+ * @param showAdultPosts Whether or not to include adult posts
+ * @returns 
+ */
+export const filterAdultPostQuery = <
+    S extends GenericSchema,
+    Rw extends Record<string, unknown>,
+    Rs
+>(
+    query: PostgrestFilterBuilder<S, Rw, Rs>,
+    showAdultPosts: boolean
+) => {
+    if (showAdultPosts) {
+        return query;
+    }else{
+        return query.eq('adult_content', false);
+    }
+};
 
 const ShowAdultToggle = ({}) => {
     const [showAdultPosts, setShowAdultPosts] = useState(false);
